8098b1caeb9a2b86c776817b6cca400ce74fefb9
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2024-11-06 13:39:16 +0700

    tutor 7 done

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
   final String npm = '5000000000'; // NPM
@@ -20,6 +22,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       // AppBar adalah bagian atas halaman yang menampilkan judul.
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
         // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
         title: const Text(
           'Mental Health Tracker',
@@ -31,6 +34,7 @@ class MyHomePage extends StatelessWidget {
         // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      drawer: const LeftDrawer(),
       // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -128,63 +132,73 @@ class InfoCard extends StatelessWidget {
   }
 }
 
-class ItemHomepage {
-  final String name;
-  final IconData icon;
 
-  ItemHomepage(this.name, this.icon);
-}
 
-class ItemCard extends StatelessWidget {
-  // Menampilkan kartu dengan ikon dan nama.
 
-  final ItemHomepage item;
 
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Menentukan warna latar belakang dari tema aplikasi.
-      color: Theme.of(context).colorScheme.secondary,
-      // Membuat sudut kartu melengkung.
-      borderRadius: BorderRadius.circular(12),
-
-      child: InkWell(
-        // Aksi ketika kartu ditekan.
-        onTap: () {
-          // Menampilkan pesan SnackBar saat kartu ditekan.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-                SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
-        },
-        // Container untuk menyimpan Icon dan Text
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              // Menyusun ikon dan teks di tengah kartu.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-}
+// class ItemHomepage {
+//   final String name;
+//   final IconData icon;
+//
+//   ItemHomepage(this.name, this.icon);
+// }
+//
+// class ItemCard extends StatelessWidget {
+//   // Menampilkan kartu dengan ikon dan nama.
+//
+//   final ItemHomepage item;
+//
+//   const ItemCard(this.item, {super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       // Menentukan warna latar belakang dari tema aplikasi.
+//       color: Theme.of(context).colorScheme.secondary,
+//       // Membuat sudut kartu melengkung.
+//       borderRadius: BorderRadius.circular(12),
+//
+//       child: InkWell(
+//         // Aksi ketika kartu ditekan.
+//         onTap: () {
+//           // Memunculkan SnackBar ketika diklik
+//           ScaffoldMessenger.of(context)
+//             ..hideCurrentSnackBar()
+//             ..showSnackBar(SnackBar(
+//                 content: Text("Kamu telah menekan tombol ${item.name}!")));
+//
+//           // Navigate ke route yang sesuai (tergantung jenis tombol)
+//           if (item.name == "Tambah Mood") {
+//             Navigator.push(context,
+//                 MaterialPageRoute(builder: (context) => const MoodEntryFormPage()));
+//
+//           }
+//         },
+//         // Container untuk menyimpan Icon dan Text
+//         child: Container(
+//           padding: const EdgeInsets.all(8),
+//           child: Center(
+//             child: Column(
+//               // Menyusun ikon dan teks di tengah kartu.
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Icon(
+//                   item.icon,
+//                   color: Colors.white,
+//                   size: 30.0,
+//                 ),
+//                 const Padding(padding: EdgeInsets.all(3)),
+//                 Text(
+//                   item.name,
+//                   textAlign: TextAlign.center,
+//                   style: const TextStyle(color: Colors.white),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+// }
